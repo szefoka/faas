094414371c634629e53a30436d5730ca7eb8352d
David Balla <szefoka@gmail.com>
2019-06-13 11:32:26 +0200

    node kubernetes yamls and build scripts added

---
+++ node/build.sh
@@ -0,0 +1,5 @@
+NAME=node-compute
+IP_PORT=$(ip a l $(route -n | awk '$1 == "0.0.0.0" {print $8}') | grep "inet" | grep -v inet6 | awk '{print $2}' | cut -d / -f 1):5000
+docker build -t $IP_PORT/$NAME .
+docker push $IP_PORT/$NAME
+sed -i "/image:/s/$/ $IP_PORT\/$NAME/" dep.yaml
