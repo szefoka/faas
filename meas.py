15fea6399ef0ba3567bf2ad7258541b5b7ac1a04
David Balla <szefoka@gmail.com>
2019-11-15 15:24:48 +0100

    Update meas.py

--- meas.py
+++ meas.py
@@ -70,13 +70,9 @@ for runtime in runtimes:
                 y = None
                 for alg in envoy_lb_algs:
                     path = runtime+"/"+t+"/"+p+"/"+alg
-<<<<<<< HEAD
                     if not os.path.exists("results/"+path):
                         os.makedirs("results/"+path)
                     nfiles = len([name for name in os.listdir("results/"+path) if os.path.isfile(os.path.join("results/"+path, name))])
-=======
-                    nfiles = len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))])
->>>>>>> a5a542bdbdd9f2d0e914a249c86243ff789dce26
                     if nfiles < len(limits):
                         with open(runtime+"/envoy/dep.yaml", "r") as f:
                             y = f.read()
@@ -99,13 +95,9 @@ for runtime in runtimes:
                 addr = istio_addr
                 for alg in istio_lb_algs:
                     path = runtime+"/"+t+"/"+p+"/"+alg
-<<<<<<< HEAD
                     if not os.path.exists("results/"+path):
                         os.makedirs("results/"+path)
                     nfiles = len([name for name in os.listdir("results/"+path) if os.path.isfile(os.path.join("results/"+path, name))])
-=======
-                    nfiles = len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))])
->>>>>>> a5a542bdbdd9f2d0e914a249c86243ff789dce26
                     if nfiles < len(limits):
                         with open(runtime+"/istio/destination_rule.yaml", "r") as f:
                             y = f.read()
