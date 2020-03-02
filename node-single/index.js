d3a34b256a1667b485b99af850fc504974aaf3bb
David Balla <szefoka@gmail.com>
2020-03-02 10:01:26 +0100

    Update index.js

--- node-single/index.js
+++ node-single/index.js
@@ -27,7 +27,7 @@ switch(func_type) {
         fptr = function (query, callback) {
             var i;
             var pi = 0;
-            for (i = 0; i < 150000; i++) {
+            for (i = 0; i < 50000; i++) {
                 _new = 4.0/(1.0+i*2.0);
                 if (i%2 != 0) {
                     pi -= _new
